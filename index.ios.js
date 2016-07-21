c0ab2370938464e269416f404bcf338774383814
Christopher Auyeung <chris@winfieldco.com>
2016-07-21 13:43:19 -0400

    updated to work in iOS

--- index.ios.js
+++ index.ios.js
@@ -1,20 +1,2 @@
-'use strict';
-
-let React = require('react-native');
-
-let {
-  Component,
-} = React;
-
-let ERROR = 'RNGLocations is not available in iOS - use Geolocation Polyfill.';
-
-class RNGLocations extends Component {
-  constructor (props) {
-    super(props);
-    console.log(ERROR);
-  }
-
-  getLocation () { console.error(ERROR) }
-}
-
-module.exports = RNGLocations;
+// RNGLocations is not available in iOS - use Geolocation Polyfill
+module.exports = {};
